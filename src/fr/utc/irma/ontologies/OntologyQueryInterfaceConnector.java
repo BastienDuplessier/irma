c811dd43d40d99fe2f6dfab6a0a365f598655e7d
Duplessier Bastien <bastien.duplessier@gmail.com>
2014-05-25 17:35:04 +0200

    Add onology object with long name cauz it's java

---
+++ src/fr/utc/irma/ontologies/OntologyQueryInterfaceConnector.java
@@ -0,0 +1,49 @@
+package fr.utc.irma.ontologies;
+
+import java.io.IOException;
+
+import android.content.res.AssetManager;
+
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+public class OntologyQueryInterfaceConnector {
+
+	private static final String UNIQUE_ONTOLOGIES_DIRECTORY_NAME = "ontologies/";
+	// UNIQUE FILENAMES FOR ONTOLOGIES
+	private static final String IRMA_GENERIC = "irma_generic.n3";
+	private static final String IRMA_SPECIFIC = "irma_cooking.n3";
+	// TODO: Put real data, not sample
+	private static final String IRMA_DATA = "irma_data.n3";
+
+	private Model model = ModelFactory.createDefaultModel();
+
+	public OntologyQueryInterfaceConnector(AssetManager assetManager) {
+		try {
+			model.read(assetManager.open(UNIQUE_ONTOLOGIES_DIRECTORY_NAME + IRMA_GENERIC), null, "TURTLE");
+			model.read(assetManager.open(UNIQUE_ONTOLOGIES_DIRECTORY_NAME + IRMA_SPECIFIC), null, "TURTLE");
+			model.read(assetManager.open(UNIQUE_ONTOLOGIES_DIRECTORY_NAME + IRMA_DATA), null, "TURTLE");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		/*
+		*/
+	}
+	
+	public void executeSparql() {
+		Query query = QueryFactory.create("PREFIX irma: <http://www.w3.org/2014/06/irma#> "
+				+ "SELECT ?recipe WHERE { ?recipe irma:linked_to irma:chorizo . }");
+		QueryExecution queryExecution = QueryExecutionFactory.create(query, model);
+		ResultSet rawResults = queryExecution.execSelect();
+		String cleanResults = ResultSetFormatter.asText(rawResults);
+		queryExecution.close();
+		System.out.println(cleanResults);
+	}
+}
