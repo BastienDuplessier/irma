32ebaaad4c9fb5d1ca2eabf9168ea377ad93080d
Puck7 <pierre.lemaire.777@gmail.com>
2014-05-26 11:54:05 +0200

    Interaction avec le bouton next
    Renan : Pourquoi tu as utilisé un fragment pour l'activité Graph ?

--- src/fr/utc/irma/MainActivity.java
+++ src/fr/utc/irma/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +13,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button nextButton;
+        
+        nextButton = (Button) findViewById(R.id.beginSearchButton);
+        
+        nextButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				Intent myIntent = new Intent(MainActivity.this, GraphActivity.class);
+				MainActivity.this.startActivity(myIntent);
+				 
+			}
+		});
     }
     
     public void startGraphActivity(View buttonClicked){
