98dc7060751c26a5ce8d3c1e8a48958160d9512c
Duplessier Bastien <bastien.duplessier@gmail.com>
2014-05-28 16:49:37 +0200

    Add recipe and recipes manager

---
+++ src/fr/utc/irma/ontologies/Recipe.java
@@ -0,0 +1,48 @@
+package fr.utc.irma.ontologies;
+
+import com.hp.hpl.jena.query.QuerySolution;
+
+public class Recipe {
+	
+	private String id;
+	private String name;
+	private String url;
+	private String imageUrl;
+	private String howMany;
+	private String preparationTime;
+	private String cookTime;
+	private String difficulty;
+	private String textIngredients;
+	private String textRecipe;
+	
+	
+	public Recipe(QuerySolution row) {
+		this.id = row.get("id").toString();
+		this.name = row.get("name").toString();
+		this.url = row.get("url").toString();
+		this.imageUrl = row.get("imageUrl").toString();
+		
+		// Recipe specific
+		this.howMany = row.get("howMany").toString();
+		this.preparationTime = row.get("preparationTime").toString();
+		this.cookTime = row.get("cookTime").toString();
+		this.difficulty = row.get("difficulty").toString();
+		this.textIngredients = row.get("textIngredients").toString();
+		this.textRecipe = row.get("textRecipe").toString();
+	}
+	
+	public String toString() {
+		return "" + this.id + " - " + this.name + " -- " + this.url + " -- " + this.imageUrl;
+	}
+	
+	// Get Methods
+	public String getId() { return this.id; }
+	public String getName() { return this.name; }
+	public String getUrl() { return this.url; }
+	public String getHowMany() { return this.howMany; }
+	public String getPreparationTime() { return this.preparationTime; }
+	public String getCookTime() { return this.cookTime; }
+	public String getDifficulty() { return this.difficulty; }
+	public String getTextIngredients() { return this.textIngredients; }
+	public String getTextRecipe() { return this.textRecipe; }
+}
