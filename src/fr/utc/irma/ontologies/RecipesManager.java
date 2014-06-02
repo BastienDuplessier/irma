2636a0be858dbaeb988abe46c7f1b71ad8a6a4b4
Duplessier Bastien <bastien.duplessier@gmail.com>
2014-06-02 16:04:14 +0200

    Add methods to use filter in queries

--- src/fr/utc/irma/ontologies/RecipesManager.java
+++ src/fr/utc/irma/ontologies/RecipesManager.java
@@ -49,4 +49,28 @@ public class RecipesManager {
 
 		return recipes;
 	}
+	
+	// Build Recipes with filters
+	public ArrayList<Recipe> getFiltered(Filter filter) {
+	    String sparqlQuery = getSPARQLQuery(filter.toSPARQLFragment());
+	    System.out.println(sparqlQuery);
+	    return fromSPARQL(sparqlQuery);
+	}
+
+    private String getSPARQLQuery(String sparqlFragment) {
+        return PREFIX + " "
+                + "SELECT ?id ?name ?url ?imageUrl ?howMany ?preparationTime ?cookTime "
+                + "?difficulty ?textIngredients ?textRecipe WHERE { "
+                + "?id a irma:Recipe . "
+                + "?id irma:name ?name . "
+                + "?id irma:url ?url . "
+                + "?id irma:image_url ?imageUrl . "
+                + "?id irma:how_many ?howMany . "
+                + "?id irma:preparation_time ?preparationTime . "
+                + "?id irma:cook_time ?cookTime . "
+                + "?id irma:difficulty ?difficulty . "
+                + "?id irma:text_ingredients ?textIngredients . "
+                + "?id irma:text_recipe ?textRecipe . "
+                + sparqlFragment + "} ";
+    }
 }
